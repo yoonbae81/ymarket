29ad16c3de5450f89d29b96fa10a50f33e816803
Yoonbae Cho <y@xcv.kr>
2019-08-17 22:34:55 +0900

    mode755

--- run.sh
+++ run.sh
@@ -4,15 +4,17 @@
 date="2019-08-16"
 symbol="/tmp/symbol-orig"
 temp="/tmp/day"
-output="$date.txt"
+output="$HOME/$date.txt"
 
 echo "Generating symbol list..."
-#~/market/symbol.py | tail -n +2 | awk 'BEGIN {FS=","}; {print $1}' > $symbol
+~/market/symbol.py | tail -n +2 | awk 'BEGIN {FS=","}; {print $1}' > $symbol
 
 count=`cat $symbol | wc -l`
 echo "Fetching daily prices of $count symbols..."
-~/market/day.py -s $symbol -o $temp
+rm -rf $temp
+cat $symbol | parallel -j 400% ~/market/day.py {} --dir $temp
 
-cat $temp | grep $date | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $output
+
+cat $temp/*.csv | grep $date | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $output
 lines=`cat $output | wc -l`
 echo "$lines prices wrote to $output"
