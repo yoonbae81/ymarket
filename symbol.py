d06592a91e7810a2595369ae2547c5555897004e
Yoonbae Cho <y@xcv.kr>
2019-08-17 14:16:17 +0900

    Create symbol.py

---
+++ symbol.py
@@ -0,0 +1,51 @@
+import re
+import scrapy
+from scrapy.exporters import CsvItemExporter
+
+RE = re.compile("code:\"(.+)\",name :\"(.+)\",cost :\"(.+)\",updn")
+
+class RawExporter(CsvItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs['include_headers_line'] = False
+        kwargs['join_multivalued'] = '_'
+
+        super(RawExporter, self).__init__(*args, **kwargs)
+
+class Pipeline(object):
+    def process_item(self, item, spider):
+        if 'price' in item:
+            item['price'] = item['price'].replace(",", "")
+        return item
+
+class SymbolSpider(scrapy.Spider):
+    name = "symbol"
+
+    custom_settings = {
+        # 'FEED_URI': 'stdout:',
+        # 'FEED_EXPORTERS': { 'csv': 'symbol.RawExporter' },
+        # 'ITEM_PIPELINES': { 'symbol.Pipeline': 0},
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'CONCURRENT_REQUESTS': 2
+    }
+
+    start_urls = [
+        'http://finance.daum.net/xml/xmlallpanel.daum?stype=P&type=S',
+        'http://finance.daum.net/xml/xmlallpanel.daum?stype=Q&type=S'
+    ]
+
+    def parse(this, response):
+        for line in re.finditer(RE, response.text):
+            yield {
+                'symbol': line.group(1),
+                'name': line.group(2),
+                'price': line.group(3).replace(",", "")
+            }
+
+
+''' debug code
+import requests
+response = requests.get("http://finance.daum.net/xml/xmlallpanel.daum?stype=P&type=S")
+
+for line in re.finditer(RE, response.text):
+    print({'symbol': line.group(1), 'name': line.group(2)})
+'''
