3ab8cbf4635b8e760615ea841ea58a2e09a810cb
Yoonbae Cho <y@xcv.kr>
2019-08-18 01:32:41 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#date=$(date '+%Y-%m-%d')
-date="2019-08-16"
-temp="/tmp/day"
-output="$HOME/yQuant.data/day/$date.txt"
+#DATE=$(date '+%Y-%m-%d')
+DATE="2019-08-16"
+TEMP="/tmp/market"
+DIR="$HOME/yQuant.data"
 
-echo "Fetching daily prices..."
-rm -rf $temp
-~/market/day_all.py -o $temp
+echo "Fetching symbols list"
+SYMBOLS="$TEMP/symbols"
+~/market/symbol.py | tail -n +2 | awk 'BEGIN {FS=","}; {print $1}' > $SYMBOLS
 
-cat $temp | grep $date | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $output
-lines=`cat $output | wc -l`
-echo "$lines prices wrote to $output"
+# output="$HOME/yQuant.data/day/$date.txt"
+#
+# echo "Fetching daily prices..."
+# rm -rf $temp
+# ~/market/day_all.py -o $temp
+#
+# cat $temp | grep $date | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $output
+# lines=`cat $output | wc -l`
+# echo "$lines prices wrote to $output"
