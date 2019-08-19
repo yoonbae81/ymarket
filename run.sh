494fc35070a3e82661b3ef7cc02af21691417074
Yoonbae Cho <y@xcv.kr>
2019-08-19 23:21:55 +0900

    update

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 DATE=$(date '+%Y-%m-%d')
 #DATE="2019-08-16"
-TEMP=$(mktemp -d /tmp/market.XXXX)
 BASE=$HOME/market
 
 ######################
@@ -15,19 +14,19 @@ echo "Symbols: `cat $SYMBOLS | wc -l`"
 ######################
 # Day
 ######################
-$TEMP=$(mktemp)
-$BASE/day.py -l $SYMBOLS -o $TEMP
-DIR=$BASE/data.day
-mkdir -p $DIR
-cat $TEMP | grep $DATE | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $DIR/$DATE.txt
-echo `wc -l $DIR/$DATE.txt`
+# TEMP=$(mktemp)
+# $BASE/day.py -l $SYMBOLS -o $TEMP
+# DIR=$BASE/data.day
+# mkdir -p $DIR
+# cat $TEMP | grep $DATE | awk 'BEGIN {FS=","}; {OFS="\t"}; {print $2,$3,$4,$5,$6,$7}' | sort > $DIR/$DATE.txt
+# echo `wc -l $DIR/$DATE.txt`
 
 ######################
 # Minute
 ######################
-$TEMP=$(mktemp)
+TEMP=$(mktemp)
 cat $SYMBOLS | parallel --jobs 20 --group $BASE/minute.py {} > $TEMP
-$DIR=$BASE/data.minute
+DIR=$BASE/data.minute
 mkdir -p $DIR
 sort -k4 $TEMP > $DIR/$DATE.txt
 echo `wc -l $DIR/$DATE.txt`
