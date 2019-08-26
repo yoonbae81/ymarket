39b8077f3723db16ae343587f8d7271c6fc910c1
Yoonbae Cho <y@xcv.kr>
2019-08-26 17:48:56 +0900

    set default value for date as today

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-	echo "No date given. e.g. YYYY-MM-DD"
-	exit 1
-fi
-
-DATE=$1
+DATE=${1:-`date +%Y-%m-%d`}
 DIR=$HOME/market
 
 ######################
@@ -21,15 +15,15 @@ wc -l $SYMBOLS | ~/bin/telegram > /dev/null
 ######################
 TEMP=$(mktemp /tmp/day.XXXX)
 cat $SYMBOLS | parallel --jobs 30 -N100 --pipe $DIR/day.py -d $DATE -f - > $TEMP
-mkdir -p $DIR/data.day
-sort $TEMP > $DIR/data.day/$DATE.txt
-wc -l $DIR/data.day/$DATE.txt | ~/bin/telegram > /dev/null
+mkdir -p $DIR/data/day
+sort $TEMP > $DIR/data/day/$DATE.txt
+wc -l $DIR/data/day/$DATE.txt | ~/bin/telegram > /dev/null
 
 ######################
 # Minute
 ######################
 TEMP=$(mktemp /tmp/minute.XXXX)
 cat $SYMBOLS | parallel --jobs 30 -N100 --pipe $DIR/minute.py -d $DATE -f - > $TEMP
-mkdir -p $DIR/data.minute
-sort -k4 $TEMP > $DIR/data.minute/$DATE.txt
-wc -l $DIR/data.minute/$DATE.txt | ~/bin/telegram > /dev/null
+mkdir -p $DIR/data/minute
+sort -k4 $TEMP > $DIR/data/minute/$DATE.txt
+wc -l $DIR/data/minute/$DATE.txt | ~/bin/telegram > /dev/null
